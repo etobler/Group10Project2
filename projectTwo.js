24bf09ad9173c09549428c7b0ca314c8f6471cc5
Emily <etobler@asu.edu>
2021-03-09 19:58:24 -0700

    Fixing survery page

---
+++ projectTwo.js
@@ -0,0 +1,9 @@
+function clickSignUp(form) {
+    // if input isn't valid pops up an error message
+    if (!form.checkValidity()) {
+        alert("Please check your input, username must be at least 3 characters, \n Password must be at least 5 characters")
+    }
+    else {
+        // do nothing 
+    }
+}
